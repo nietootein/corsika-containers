544ded8593f7a507476314690f85f967dbc2cec4
nietootein <d.nieto@ucm.es>
2020-04-30 18:04:11 +0200

    Adding images for corsika only (w and w/o fluorescence)

--- assets/install.sh
+++ assets/install.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+#Installation path
+target_path='/home/'
+
 my_asset=`ls *tar.gz`
 my_path=`basename $my_asset .tar.gz`
+my_path=$target_path/$my_path
 mkdir -p $my_path
 tar zxvf $my_asset -C $my_path
 rm $my_asset
-cd $my_path
-options='prod4 qgs2'
-echo ./build_all $options > build_all.out
-./build_all $options &>> build_all.out
+
+if [[ $my_asset == *"simtel"* ]]; then
+    #corsika-simtelarray build_all options
+    options='prod4 qgs2'
+    ln -s $my_path $target_path/corsika_simtelarray
+    cd $my_path
+    echo ./build_all $options > build_all.out
+    ./build_all $options &>> build_all.out
+else
+    ln -s $my_path $target_path/corsika
+    cd $my_path
+    tar zxvf ./bernlohr/bernlohr*tar.gz -C ./bernlohr
+    echo ./bernlohr/corsika_build_script > corsika_build_script.out
+    ./bernlohr/corsika_build_script &>> corsika_build_script.out
+fi
--- prototype/dummy.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import argparse
-
-parser = argparse.ArgumentParser(description='Square a number.')
-parser.add_argument('number', metavar='N', type=float,
-                   help='a number')
-args = parser.parse_args()
-print(np.power(args.number,2))
